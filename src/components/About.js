082b4c32657da25870d4a9f47b8f17571c9a54db
marce-moya <marcelamoya@gmail.com>
2024-05-22 01:19:46 -0400

    correccion errores

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,14 @@ export default function About() {
             <p>¡Bienvenido a nuestra tienda en línea! En nuestro catálogo encontrarás una selección de productos altamente necesarios para tu mascota.</p>
             <div>
                 <img src={imagen} alt="Nuestra tienda con los indispensables" />
-                <p>Este es un párrafo adicional que puedes agregar para proporcionar más información sobre tu tienda o tu equipo.</p>
+                <p>¡Bienvenidos a Nombre de la Tienda!</p>
+                <p>En Pet Shop Animal Care, sabemos lo importante que son tus mascotas para ti. Son parte de la familia y merecen solo lo mejor. Es por eso que nos dedicamos a ofrecer una amplia variedad de productos de alta calidad para satisfacer todas las necesidades de tus queridos compañeros.  </p>
+                <p>Desde camas acogedoras y areneros prácticos hasta juguetes divertidos y nutritiva comida, en nuestra tienda encontrarás todo lo que tu mascota necesita para vivir feliz y saludable. Nos enorgullece ofrecer productos que no solo cumplen con altos estándares de calidad, sino que también son asequibles y accesibles para todos los amantes de las mascotas. </p>
+                <p>Nuestro compromiso:</p>
+                <p>En Pet Shop Animal Care, no solo vendemos productos, sino que también estamos comprometidos con el bienestar de tus mascotas. Nuestro equipo está siempre disponible para ayudarte a encontrar el producto perfecto y ofrecerte consejos expertos sobre el cuidado de tus animales. Nos esforzamos por ser más que una tienda, queremos ser tu socio en el cuidado de tus mascotas.</p>
+                <p>Conéctate con Nosotros</p>
+                <p>Sigue nuestro blog para obtener consejos útiles, novedades sobre productos y promociones exclusivas. ¡Únete a nuestra comunidad de amantes de las mascotas y comparte tus experiencias con nosotros!</p>
+                <p>Gracias por confiar en Pet Shop Animal Care. Juntos, podemos asegurar que tus mascotas reciban el amor y el cuidado que merecen.</p>
             </div>
   
      
